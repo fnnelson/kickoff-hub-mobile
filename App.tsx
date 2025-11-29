560882f2bae3b8612f776131b7c505cbb9fe84fb
Forrest Nelson <forrestnnelson@gmail.com>
2025-11-28 18:37:33 -0600

    Initial setup for iPhone and Android

--- App.tsx
+++ App.tsx
@@ -1,5 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+// to add react navigation
+// https://reactnavigation.org/docs/bottom-tab-navigator/?config=dynamic
+// const Tab = createBottomTabNavigator();
+
+// function MyTabs() {
+//   return (
+//     <Tab.Navigator>
+//       <Tab.Screen name="Home" component={HomeScreen} />
+//       <Tab.Screen name="Profile" component={ProfileScreen} />
+//     </Tab.Navigator>
+//   );
+// }
 
 export default function App() {
   return (
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Kickoff Hub Mobile
+
+Kickoff Hub color pallette: 
+green - #3a7259
+yellow - #fadf5e
+black - #383838
+white - #f7f7f7
